9f97370fddcb0ff6e24998c9ea9b1bec3455b6ea
Zuan0x <zuan.0x@gmail.com>
2023-10-24 13:17:43 +0100

    Add bst class

---
+++ binary-search-tree.ts
@@ -0,0 +1,24 @@
+class TreeNode {
+    constructor(public value: number, public left?: TreeNode, public right?: TreeNode) {
+        this.value = value;
+        this.left = undefined;
+        this.right = undefined;
+    }
+}
+
+class BinarySearchTree {
+    constructor(public root?: TreeNode) {
+        this.root = undefined
+    }
+
+    //Functions
+    //Insert
+    //Delete
+
+    //Helper functions
+    //Find min node
+    //Find max node
+    //Search
+    //Get root node
+    //Get height of tree
+}
